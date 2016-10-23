adef0ae5fc7f11e20d79e38cb15e4976038da9b1
Andrey Romanov <steampart@gmail.com>
2016-10-23 13:43:24 +0300

    Changed typos

--- 20.10.2016/Human.java
+++ 20.10.2016/Human.java
@@ -2,11 +2,11 @@
 
 public class Human extends Monkey{
     
-	 public int calculateIQ(int points){// override of method in Monkey class
+	   public int calculateIQ(int points){// override of method in Monkey class
 
-    	return points + 50;
+    	    return points + 50;
 
-     }
+         }
 
      public String getColor(){// override of method in Monkey class
 
