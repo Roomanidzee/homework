418f0265a3470258c1061457df25e9ca8182ab11
Andrey Romanov <steampart@gmail.com>
2017-05-17 18:33:19 +0300

    Added homework for 16.05.2017

---
+++ 2nd Semester/16.05.2017/src/interfaces/CalcInterface.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+/**
+ *
+ * @author Андрей Романов <steampart@gmail.com>
+ */
+public interface CalcInterface {
+    
+    double addNewCost(double previousResult, double difference);
+    double subtractNewCost(double prebiousResult, double difference);
+    
+}
---
+++ 2nd Semester/16.05.2017/src/processing/LaunchFunction.java
@@ -0,0 +1,46 @@
+package processing;
+
+import runnables.CalcRunnable;
+
+/**
+ *
+ * @author Андрей Романов <steampart@gmail.com>
+ */
+public class LaunchFunction {
+    
+    public static void launch(){
+        
+        CalcRunnable calcRun = new CalcRunnable();
+        
+        Thread t1 = new Thread(calcRun);
+        Thread t2 = new Thread(calcRun);
+        Thread t3 = new Thread(calcRun);
+        Thread t4 = new Thread(calcRun);
+        Thread t5 = new Thread(calcRun);
+        
+        try {
+            
+            t1.start();
+            Thread.sleep(1000);
+            
+            t2.start();
+            Thread.sleep(1000);
+            
+            t3.start();
+            Thread.sleep(1000);
+            
+            t4.start();
+            Thread.sleep(1000);
+            
+            t5.start();
+            Thread.sleep(1000);
+            
+        } catch (InterruptedException ex) {
+            
+            System.err.println("Поток ожидания был прерван");
+            
+        }
+        
+    }
+    
+}
